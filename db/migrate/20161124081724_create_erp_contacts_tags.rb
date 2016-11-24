90a8fab5fdc17bde3efff30e99b79d506f013c9e
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-11-24 01:48:30 -0800

    add migration erp_contacts_tags

---
+++ db/migrate/20161124081724_create_erp_contacts_tags.rb
@@ -0,0 +1,10 @@
+class CreateErpContactsTags < ActiveRecord::Migration[5.0]
+  def change
+    create_table :erp_contacts_tags do |t|
+      t.string :name
+      t.references :user, index: true, references: :erp_users
+
+      t.timestamps
+    end
+  end
+end
