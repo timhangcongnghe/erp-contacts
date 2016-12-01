91f163b95ca6220329bbe249905c09da4c8898fc
Hung Nguyen <hungnt@hoangkhang.com.vn>
2016-12-01 00:03:32 -0800

    update class const + DEF self.get_type_options

--- app/models/erp/contacts/contact.rb
+++ app/models/erp/contacts/contact.rb
@@ -1,4 +1,14 @@
 module Erp::Contacts
   class Contact < ApplicationRecord
+    # class const
+    TYPE_PERSON = 'person'
+    TYPE_COMPANY = 'company'
+    
+    def self.get_type_options()
+      [
+        {text: I18n.t('contacts.contact.individual'),value: self::TYPE_PERSON},
+        {text: I18n.t('contacts.contact.company'),value: self::TYPE_COMPANY}
+      ]
+    end
   end
 end
