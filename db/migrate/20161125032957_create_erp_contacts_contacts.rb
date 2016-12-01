3fdafa7dee41f7227681b0a014cc041bc6fac07e
Hung Nguyen <hungnt@hoangkhang.com.vn>
2016-11-30 17:26:41 -0800

    update contact form, migration

--- db/migrate/20161125032957_create_erp_contacts_contacts.rb
+++ db/migrate/20161125032957_create_erp_contacts_contacts.rb
@@ -1,6 +1,22 @@
 class CreateErpContactsContacts < ActiveRecord::Migration[5.0]
   def change
     create_table :erp_contacts_contacts do |t|
+      t.string :name
+      t.string :image_url
+      t.string :type
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :zip
+      t.string :website
+      t.string :job_position
+      t.string :phone
+      t.string :mobile
+      t.string :fax
+      t.string :email
+      t.string :birthday
+      t.text :internal_note
+      t.references :user, index: true, references: :erp_users
 
       t.timestamps
     end
