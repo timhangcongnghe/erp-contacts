fab5e0aac24572f882c085524ec34c92e2ce1adf
Hung Nguyen <hungnt@hoangkhang.com.vn>
2016-12-02 20:26:08 -0800

    replace Son-code to current code

--- app/models/erp/contacts/contact.rb
+++ app/models/erp/contacts/contact.rb
@@ -13,10 +13,12 @@ module Erp::Contacts
     
     # Filters
     def self.filter(query, params)
+      params = params.to_unsafe_hash
       and_conds = []
+      
       #filters
       if params["filters"].present?
-        params["filters"].to_unsafe_h.each do |ft|
+        params["filters"].each do |ft|
           or_conds = []
           ft[1].each do |cond|
             or_conds << "#{cond[1]["name"]} = '#{cond[1]["value"]}'"
@@ -24,16 +26,18 @@ module Erp::Contacts
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
+      
       #keywords
       if params["keywords"].present?
-        params["keywords"].to_unsafe_h.each do |kw|
+        params["keywords"].each do |kw|
           or_conds = []
           kw[1].each do |cond|
-            or_conds << "LOWER(#{cond[1]["name"]}) LIKE '#{cond[1]["value"]}.downcase.strip'"
+            or_conds << "LOWER(#{cond[1]["name"]}) LIKE '%#{cond[1]["value"].downcase.strip}%'"
           end
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
+
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
       
       return query
@@ -42,7 +46,7 @@ module Erp::Contacts
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-
+      
       return query
     end
     
