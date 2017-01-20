682a229ab4d63b8cba78e24004a4f2a51b9ca2ba
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-01-19 20:29:12 -0800

    helper display contact address for contact

--- app/helpers/erp/contacts/application_helper.rb
+++ app/helpers/erp/contacts/application_helper.rb
@@ -1,6 +1,16 @@
 module Erp
   module Contacts
     module ApplicationHelper
+      
+      #helper /display contact address for contact
+      def display_contact_address(contact)
+        str = []
+        str << contact.address if contact.address?
+        str << contact.state_name if contact.state_name.present?
+        str << contact.country_name if contact.country_name.present?
+        return str.join(", ")
+      end
+      
     end
   end
 end
