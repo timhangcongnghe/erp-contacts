0f36907302f383716bf31ad05ee8ce4cf512f5a8
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-02-27 22:25:08 -0800

    fix cancan: update

--- app/decorators/models/ability_decorator.rb
+++ app/decorators/models/ability_decorator.rb
@@ -2,7 +2,7 @@ Erp::Ability.class_eval do
   def contacts_ability(user)
     can :read, Erp::Contacts::Contact
     can :update, Erp::Contacts::Contact do |contact|
-      contact.user_id == user.id
+      contact.creator == user
     end
   end
 end
