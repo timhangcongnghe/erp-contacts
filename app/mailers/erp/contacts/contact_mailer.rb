9099de6b4c4e1fb989da5cca723ef125bae5690c
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-29 23:34:12 -0700

    fix content mailer for contact us

--- app/mailers/erp/contacts/contact_mailer.rb
+++ app/mailers/erp/contacts/contact_mailer.rb
@@ -2,7 +2,7 @@ module Erp::Contacts
   class ContactMailer < Erp::ApplicationMailer
     def sending_email_contact(msg)
       @msg = msg
-      send_email(@msg.to_contact.email, "[OnlineStore.com] Bạn có một yêu cầu liên hệ/góp ý cần được xử lý")
+      send_email(@msg.to_contact.email, "[TimHangCongNghe.Vn] Bạn có một yêu cầu liên hệ/góp ý cần được xử lý")
     end 
   end
 end
