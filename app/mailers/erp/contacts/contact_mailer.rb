5687c44cf10ef9ac46cf5cf74113c862b000f29e
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-05-08 01:34:00 -0700

    hash code for recipient emails and change email subject

--- app/mailers/erp/contacts/contact_mailer.rb
+++ app/mailers/erp/contacts/contact_mailer.rb
@@ -2,7 +2,11 @@ module Erp::Contacts
   class ContactMailer < Erp::ApplicationMailer
     def sending_email_contact(msg)
       @msg = msg
-      send_email(@msg.to_contact.email, "[TimHangCongNghe.Vn] Bạn có một yêu cầu liên hệ/góp ý cần được xử lý")
+      
+      #@todo static emails
+      @recipients = ['Hùng Nguyễn <hungnt@hoangkhang.com.vn>', 'Luân Phạm <luanpm@hoangkhang.com.vn>', 'Sơn Nguyễn <sonnn@hoangkhang.com.vn>']
+      
+      send_email(@recipients.join("; "), "Nội dung tin nhắn liên hệ/góp ý từ #{@msg.contact.email}")
     end 
   end
 end
