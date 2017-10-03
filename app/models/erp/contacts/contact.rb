4a670ec69546a6ebbb262be0f185d8fbfbd4baa7
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-10-04 00:18:52 +0700

    update amount for contact sales orders

--- app/models/erp/contacts/contact.rb
+++ app/models/erp/contacts/contact.rb
@@ -259,5 +259,54 @@ module Erp::Contacts
     def must_select_is_customer_or_supplier
       errors.add(:is_customer, :message_must_select) unless (is_customer == true or is_supplier == true)
     end
+    
+    # todo: check order/payment engine available?
+    # Get sales orders for contact (is ordered)
+    def sales_orders
+      Erp::Orders::Order.sales_orders.where(customer_id: self.id)
+                        .where(status: Erp::Orders::Order::STATUS_CONFIRMED)
+    end
+    
+    def sales_orders_is_payment_for_contact
+      sales_orders.where(payment_for: Erp::Payments::PaymentType::TYPE_FOR_CONTACT)
+    end
+    
+    # Sales total amount for contact
+    def sales_total_amount(params={})
+      query = self.sales_orders_is_payment_for_contact
+      
+      if params[:from].present?
+        query = query.where('order_date >= ?', params[:from].beginning_of_day)
+      end
+      
+      if params[:to].present?
+        query = query.where('order_date <= ?', params[:to].end_of_day)
+      end
+      
+      return query.sum(:cache_total)
+    end
+    
+    # Sales paid amount for contact
+    def sales_paid_amount(params={})
+      query = Erp::Payments::PaymentRecord.where(status: Erp::Payments::PaymentRecord::STATUS_DONE)
+                                          .includes(:payment_type)
+                                          .where(erp_payments_payment_types: {code: Erp::Payments::PaymentType::TYPE_FOR_CONTACT})
+                                          .where(contact_id: self.id)
+      if params[:from].present?
+        query = query.where('payment_date >= ?', params[:from].beginning_of_day)
+      end
+      
+      if params[:to].present?
+        query = query.where('payment_date <= ?', params[:to].end_of_day)
+      end
+      
+      return query.sum(:amount)
+    end
+    
+    # Sales debt amount for contact
+    def sales_debt_amount(params={})
+      self.sales_total_amount(params) - self.sales_paid_amount(params)
+    end
+    
   end
 end
