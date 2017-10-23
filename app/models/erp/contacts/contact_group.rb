e74af2ea9a3c23633d384197d56e8d6456dd2024
Luan Pham <luanpm88@gmail.com>
2017-10-23 00:55:39 -0700

    remove some required field

--- app/models/erp/contacts/contact_group.rb
+++ app/models/erp/contacts/contact_group.rb
@@ -1,13 +1,13 @@
 module Erp::Contacts
   class ContactGroup < ApplicationRecord
-    validates :name, :discount, :discount_type, :presence => true
+    validates :name, :presence => true
     belongs_to :creator, class_name: "Erp::User", optional: true
     has_many :contact_group_conditions
     accepts_nested_attributes_for :contact_group_conditions, :reject_if => lambda { |a| a[:name].blank? || a[:operator].blank? || a[:value].blank?}, :allow_destroy => true
-    
+
     DISCOUNT_TYPE_FIXED = 'fixed'
     DISCOUNT_TYPE_PERCENT = 'percent'
-    
+
     # get discount type
     def self.get_discount_type_options()
       [
@@ -15,13 +15,13 @@ module Erp::Contacts
         {text: I18n.t('fixed'), value: 'fixed'}
       ]
     end
-    
+
     # Filters
     def self.filter(query, params)
       params = params.to_unsafe_hash
       and_conds = []
       show_archived = false
-      
+
       #filters
       if params["filters"].present?
         params["filters"].each do |ft|
@@ -37,10 +37,10 @@ module Erp::Contacts
           and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
         end
       end
-      
+
       # show archived items condition - default: false
       show_archived = false
-      
+
       #filters
       if params["filters"].present?
         params["filters"].each do |ft|
@@ -57,7 +57,7 @@ module Erp::Contacts
           and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
         end
       end
-      
+
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -71,55 +71,55 @@ module Erp::Contacts
 
       # join with users table for search creator
       query = query.joins(:creator)
-      
+
       # showing archived items if show_archived is not true
       query = query.where(archived: false) if show_archived == false
-      
+
       # add conditions to query
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
-      
+
       return query
     end
-    
+
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-      
+
       # order
       if params[:sort_by].present?
         order = params[:sort_by]
         order += " #{params[:sort_direction]}" if params[:sort_direction].present?
-        
+
         query = query.order(order)
       end
-      
+
       return query
     end
-    
+
     # data for dataselect ajax
     def self.dataselect(keyword='')
       query = self.all
-      
+
       if keyword.present?
         keyword = keyword.strip.downcase
         query = query.where('LOWER(name) LIKE ?', "%#{keyword}%")
       end
-      
+
       query = query.limit(8).map{|contact_group| {value: contact_group.id, text: contact_group.name} }
     end
-    
+
     def archive
 			update_attributes(archived: true)
 		end
-    
+
     def unarchive
 			update_attributes(archived: false)
 		end
-    
+
     def self.archive_all
 			update_all(archived: true)
 		end
-    
+
     def self.unarchive_all
 			update_all(archived: false)
 		end
